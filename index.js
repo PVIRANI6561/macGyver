116360d49f8c8a78ed4a90a6da68f62184617c06
PVIRANI6561 <pvirani6561@gmail.com>
2022-04-08 12:15:51 +0530

    Port changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path')
 const requests = require('requests')
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/templates/views'))
 hbs.registerPartials(path.join(__dirname, '/templates/partials'))
@@ -35,6 +37,6 @@ app.get("*", (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('done');
 })
